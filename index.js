cee159285fd5ec3cf28010a0192f56dd2e46db66
mistermur <brian.j.murillo@gmail.com>
2019-02-17 17:27:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+ customerName = 'bob'
+ var bestCustomer;
+
+function upperCaseCustomerName()
+{
+  return customerName=customerName.toUpperCase()
+};
+function setBestCustomer()
+{
+  bestCustomer = `not ${customerName.toLowerCase()}`
+  console.log(bestCustomer)
+  return bestCustomer
+};
+function overwriteBestCustomer()
+{
+  bestCustomer = `maybe ${customerName.toLowerCase()}`
+  // return bestCustomer
+};
+function changeLeastFavoriteCustomer()
+{
+  alert.error(TypeError)
+}
